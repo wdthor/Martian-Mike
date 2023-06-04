48f5bcbfa1434f13041b21b76a0fc69ef7671b74
wdthor <wdthor@proton.me>
2023-06-04 04:28:22 +0200

    3. Player movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,1 +1,23 @@
 extends CharacterBody2D
+
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _physics_process(delta):
+	if !is_on_floor():
+		velocity.y += gravity * delta
+#		Prevents increasing the velocity more
+		if velocity.y > 500:
+			velocity.y = 500
+		print(velocity.y)
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += -jump_force
+
+#	Input.get_axis will return -1 if move_left, 1 if move_right, 0 if none or both of them
+	var direction = Input.get_axis("move_left", "move_right")
+	velocity.x = direction * speed
+	move_and_slide()
