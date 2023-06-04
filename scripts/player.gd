bcfe0a309c2ce98a9d3ceb455f3fdd78ac62bed4
wdthor <wdthor@proton.me>
2023-06-04 04:55:16 +0200

    5. Updating animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -19,5 +19,24 @@ func _physics_process(delta):
 
 #	Input.get_axis will return -1 if move_left, 1 if move_right, 0 if none or both of them
 	var direction = Input.get_axis("move_left", "move_right")
+#	Flip the sprite left if direction == -1 or flip right
+#	If no key is pressed, direction = 0
+	if direction != 0:
+		animated_sprite.flip_h = direction == -1
 	velocity.x = direction * speed
 	move_and_slide()
+	
+	update_animations(direction)
+	
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+		
