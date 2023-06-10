f39868b2d835275e213a09ed34026ab00a0a5c7c
wdthor <wdthor@proton.me>
2023-06-10 07:18:50 +0200

    8. Traps * Add saw and spike traps scenes

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var start_position = $StartPosition
 
+@onready var player = $Player
+
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
@@ -12,5 +14,12 @@ func _on_deathzone_body_entered(body):
 	reset_position(body)
 	
 func reset_position(body):
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
+	reset_player()
+
+
+func _on_trap_touched_player():
+	reset_player()
+	
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
---
+++ scripts/trap.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+signal touched_player
+
+func _on_area_2d_body_entered(body):
+	if body is Player:
+#		1st way to emit signal
+#		emit_signal("touched_player")
+#		2nd (new) way to emit signal
+		touched_player.emit()
