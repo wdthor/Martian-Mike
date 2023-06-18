fb3b761428f8c6eed2b033b72a81e401ef2e9e20
wdthor <wdthor@proton.me>
2023-06-18 07:54:54 +0200

    15. Start area * Remove previous start position * Assign new scene with spawn position as start position * Add player in Groups player

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
-@onready var start_position = $StartPosition
+@onready var start = $Start
 
-@onready var player = $Player
+var player = null
 
 func _ready():
-	# Get all nodes tagged as "traps"
+	# Get first node tagged as "player" (created in Groups)
+	# And assign it to player
+	# Note : another way of doing @onready
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start.get_spawn_position()
+
+	# Get nodes tagged as "traps" (created in Groups)
 	var traps = get_tree().get_nodes_in_group("traps")
 	for trap in traps:
 #		1st way to connect
@@ -31,4 +38,4 @@ func _on_trap_touched_player():
 	
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_position()
